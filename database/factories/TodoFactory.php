4e45d64f0d2d0e0a1c66531a8bf8c533fed35d5a
Maksym Grom <MailMaximGrom@gmail.com>
2019-01-19 15:05:38 +0200

    fixed db issue

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Todo::class, function (Faker $faker) {
     return [
-        'text' => $faker->text(),
+        'text' => $faker->text(70),
         'completed' => $faker->boolean(20)
     ];
 });
